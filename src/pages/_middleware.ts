02ca78d9ef262b317e96f0bdfe7e936f30bf382e
yshir <yshir0020@gmail.com>
2022-01-29 22:14:30 +0900

    Add verifyEnv, verifyQuery

--- src/pages/_middleware.ts
+++ src/pages/_middleware.ts
@@ -1,20 +1,8 @@
 import { NextRequest, NextResponse } from "next/server";
 
-const USERNAME = "foo";
-const PASSWORD = "bar";
-
 export const middleware = (req: NextRequest): NextResponse | Response => {
-  const basicAuth = req.headers.get("authorization");
-
-  if (basicAuth) {
-    const auth = basicAuth.split(" ")[1];
-    const [username, password] = Buffer.from(auth, "base64")
-      .toString()
-      .split(":");
-
-    if (username === USERNAME && password === PASSWORD) {
-      return NextResponse.next();
-    }
+  if (verifyEnv() || verifyQuery(req) || verifyHeader(req)) {
+    return NextResponse.next();
   }
 
   return new Response("Auth required", {
@@ -23,4 +11,31 @@ export const middleware = (req: NextRequest): NextResponse | Response => {
       "WWW-Authenticate": 'Basic realm="Secure Area"',
     },
   });
+};
+
+// NO_AUTH=1 npm run dev
+const verifyEnv = (): boolean => {
+  return !!process.env.NO_AUTH;
+};
+
+// http://localhost:3000?no_auth=1
+const verifyQuery = (req: NextRequest): boolean => {
+  const query = Object.fromEntries(
+    new URLSearchParams(req.nextUrl.search).entries()
+  );
+
+  return !!query.no_auth;
+};
+
+const verifyHeader = (req: NextRequest): boolean => {
+  const basicAuth = req.headers.get("authorization");
+  if (!basicAuth) {
+    return false;
+  }
+
+  const auth = basicAuth.split(" ")[1];
+  const [username, password] = Buffer.from(auth, "base64")
+    .toString()
+    .split(":");
+  return username === "foo" && password === "bar";
 };
